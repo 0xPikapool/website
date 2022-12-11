004ec2fdf29576f510ab8bf318179e078127a835
braixen <pikapool-braixen@protonmail.com>
2022-12-11 21:35:09 +0200

    landing page improvements

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,8 +1,7 @@
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
-
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
@@ -12,37 +11,37 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'what',
-    imgSrc: '/img/nft_mempool.png',
+    title: "bleeding edge",
+    imgSrc: "/img/nft_mempool.png",
+    description: (
+      <>The mempool built for NFT’s... a shocking development for the space.</>
+    ),
+  },
+  {
+    title: "mev centric",
+    imgSrc: "/img/polka_sniper.png",
     description: (
       <>
-        The mempool built for NFT’s... a shocking development for the space.
+        Currently built for snipers (pew pew) but building for NFT projects
+        next.
       </>
     ),
   },
   {
-    title: 'h',
-    imgSrc: '/img/nft_mempool.png',
+    title: "solving real problems",
+    imgSrc: "/img/pika_bills.png",
     description: (
       <>
-        Currently built for snipers (pew pew) but building for NFT projects next.
-      </>
-    ),
-  },
-  {
-    title: 'a',
-    imgSrc: '/img/nft_mempool.png',
-    description: (
-      <>
-        You built the hype, you get the auction proceeds! Can I hear a bid from the man
+        You built the hype, you get the auction proceeds! Can I hear a bid from
+        the man!
       </>
     ),
   },
 ];
 
-function Feature({title, imgSrc, description}: FeatureItem) {
+function Feature({ title, imgSrc, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <img className={styles.featureSvg} role="img" src={imgSrc} />
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,45 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import { ShakeLittle } from "reshake";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            {"<<catch 'em all>>"}
-          </Link>
+          <ShakeLittle fixed={true} fixedStop={true} freez={false}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro"
+            >
+              {"<<catch 'em all>>"}
+            </Link>
+          </ShakeLittle>
         </div>
       </div>
       <div className="container">
-        <img src={"/img/pikapool.png"} style={{height: "300px"}} />
+        <img src={"/img/pikapool.png"} style={{ height: "300px" }} />
       </div>
     </header>
   );
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
