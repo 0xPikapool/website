64c804674b4a04c8dfc78350edcae2968f87f55d
braixen <pikapool-braixen@protonmail.com>
2022-12-14 15:19:56 +0200

    fix navbar logo flicker

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/pikapool.png',
+  favicon: 'img/pikapool-sm.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
